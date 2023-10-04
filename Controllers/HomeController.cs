99715a0789061810ddc6ff39e90f25ac14f04af7
Steven <juansf_456@hotmail.com>
2023-10-03 22:59:22 -0500

    Subiendo CRUD

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using datatable_core.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace datatable_core.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly string cadenaSQL;
+
+        public HomeController(IConfiguration config)
+        {
+            cadenaSQL = config.GetConnectionString("CadenaSQL");
+        }
+
+        [HttpGet]
+        public JsonResult ListaEmpleados()
+        {
+            List<Empleado> lista = new List<Empleado>();
+
+            using (var conexion = new SqlConnection(cadenaSQL))
+            {
+                conexion.Open();
+                var cmd = new SqlCommand("sp_lista_empleados", conexion);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                using (var dr = cmd.ExecuteReader())
+                {
+                    while (dr.Read())
+                    {
+                        lista.Add(new Empleado
+                        {
+                            IdEmpleado = Convert.ToInt32(dr["IdEmpleado"]),
+                            Nombre = dr["Nombre"].ToString(),
+                            Cargo = dr["Cargo"].ToString(),
+                            Oficina = dr["Oficina"].ToString(),
+                            Salario = Convert.ToInt32(dr["Salario"]),
+                            Telefono = dr["Telefono"].ToString(),
+                            FechaIngreso = dr["FechaIngreso"].ToString()
+                        });
+                    }
+                }
+            }
+
+            return Json(new {data = lista});
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
